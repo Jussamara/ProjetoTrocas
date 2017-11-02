f64285d97bfc2df7886dee780d07b1761cc8bcf4
Thiago Dorneles <thiagodornelesrs@gmail.com>
2017-11-02 13:09:47 -0200

    melhorando padrão do estilo dos formulários. Ainda falta

--- src/app/form-produtos/form-produtos.component.ts
+++ src/app/form-produtos/form-produtos.component.ts
@@ -1,8 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { CrudProdutosService} from "app/crud-produtos.service";
-import { Produto } from "app/produto";
-import { AuthService } from "app/login-form/auth.service";
-import { Router, ActivatedRoute } from "@angular/router";
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  CrudProdutosService
+} from "app/crud-produtos.service";
+import {
+  Produto
+} from "app/produto";
+import {
+  AuthService
+} from "app/login-form/auth.service";
+import {
+  Router,
+  ActivatedRoute
+} from "@angular/router";
 @Component({
   selector: 'app-form-produtos',
   templateUrl: './form-produtos.component.html',
@@ -16,9 +28,9 @@ export class FormProdutosComponent implements OnInit {
   msgErro: string;
 
   constructor(private servico: CrudProdutosService,
-              private authService: AuthService,
-              private router: Router,
-              private rota: ActivatedRoute ) { }
+    private authService: AuthService,
+    private router: Router,
+    private rota: ActivatedRoute) {}
 
   ngOnInit() {
     this._id = this.rota.snapshot.params['cod'];
@@ -29,6 +41,7 @@ export class FormProdutosComponent implements OnInit {
         error => this.msgErro = error)
     }
   }
+
   salvarProduto() {
     if (!this._id) {
       this.produto.dono = this.authService.idDoUsuarioLogado();
@@ -37,19 +50,33 @@ export class FormProdutosComponent implements OnInit {
         produto => this.router.navigate(['/lista']),
         error => this.msgErro = error
       );
-      alert ("Troca " + this.produto.nome + " adicionado com sucesso!");
-        this.produto = new Produto();
-  }
-  else {
-    this.servico.atualizaProduto(this._id, this.produto).subscribe(
-      data => this.router.navigate(['/lista']));
+      alert("Troca " + this.produto.nome + " adicionado com sucesso!");
+      this.produto = new Produto();
+    } else {
+      this.servico.atualizaProduto(this._id, this.produto).subscribe(
+        data => this.router.navigate(['/lista']));
       error => this.msgErro = error
 
-      alert("Móvel "+this.produto.nome + " editado com sucesso!");
+      alert("Móvel " + this.produto.nome + " editado com sucesso!");
+    }
+  }
+
+  voltar() {
+    this.router.navigate(['/lista']);
+  }
+
+  selecionaImagem($event) {
+    this.lerConteudoImagem($event.target);
+  }
+
+  lerConteudoImagem(inputValue){
+    const file = inputValue.files[0];
+    const myReader = new FileReader();
+
+    myReader.onloadend = (e) => {
+      this.produto.image = myReader.result;
+    };
+
+    myReader.readAsDataURL(file);
   }
 }
-    voltar() {
-      this.router.navigate(['/lista']);
-    }
-}
-
