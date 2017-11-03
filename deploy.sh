d485dcb2725ffcbcc62733cb6ae738726ef2f831
Thiago Dorneles <thiagodornelesrs@gmail.com>
2017-11-02 22:19:57 -0200

    atualizado script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cp -rf /dist /server/dist
+mv dist/ server/
 git add server
 git commit -m 'Atualizando servidor'
 git subtree push --prefix server heroku master
-
-rm -rf server/dist/
-rm -rf dist/
